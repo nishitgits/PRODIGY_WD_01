a9d7badaf7218685fba00bfa74564bb587933ccf
Nishit Kumar Sahoo <148433174+nishitgits@users.noreply.github.com>
2023-10-28 13:35:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to change the navbar style on scroll
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
